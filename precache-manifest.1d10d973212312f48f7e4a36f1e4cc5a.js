719adbd5841396f6388018ac8871681a980019af
Jack Jiang <jackjyq@outlook.com>
2019-07-18 07:10:01 +1000

    Updates

---
+++ precache-manifest.1d10d973212312f48f7e4a36f1e4cc5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd11cccb51c272f78f1d70cf02017d41",
+    "url": "/fullstack_tutorial/index.html"
+  },
+  {
+    "revision": "f191c2ab0cd151a26fe1",
+    "url": "/fullstack_tutorial/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7d176b680f3f65e69080",
+    "url": "/fullstack_tutorial/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f191c2ab0cd151a26fe1",
+    "url": "/fullstack_tutorial/static/js/2.97ea9d12.chunk.js"
+  },
+  {
+    "revision": "7d176b680f3f65e69080",
+    "url": "/fullstack_tutorial/static/js/main.4565ab02.chunk.js"
+  },
+  {
+    "revision": "efd39cd367782ab5cbc5",
+    "url": "/fullstack_tutorial/static/js/runtime~main.e079487a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fullstack_tutorial/precache-manifest.1d10d973212312f48f7e4a36f1e4cc5a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fullstack_tutorial/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
