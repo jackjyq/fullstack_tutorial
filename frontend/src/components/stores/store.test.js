07cadac826f12a11e2ca4c672333c0d7c57fbe95
player654012 <Q654012606@163.com>
2021-11-27 15:40:50 -0500

    have a try

--- frontend/src/components/stores/store.test.js
+++ frontend/src/components/stores/store.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import customer from './store';
+import store from './store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
